a88c121b9ab1891709b7c36814a9f1e3fd434916
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-12-03 15:17:59 +0300

    Finish : Module #6. Task #1. Class creation

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,54 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
+
+    public Bird() {
+
+        super.setColor("blue");
+
+        super.setNumberOfPaws(2);
+
+        super.setHasFur(false);
+
+    }
+
+    @Override
+    public String getDescription() {
+
+        String colorOne = super.getColor();
+
+        int numberOfPawsOne  = super.getNumberOfPaws();
+
+        String numberOfPawsTwo = null;
+
+        if(numberOfPawsOne == 1) {
+
+            numberOfPawsTwo = "paw";
+
+        } else {
+
+            numberOfPawsTwo = "paws";
+
+        }
+
+        boolean hasFur1 = super.getHasFur();
+
+        String hasFurTwo = null;
+
+        if(hasFur1 == true) {
+
+            hasFurTwo = "a";
+
+        } else {
+
+            hasFurTwo = "no";
+
+        }
+
+        String animal = "This animal is mostly " + colorOne + "." + " It has " + numberOfPawsOne + " " + numberOfPawsTwo + " and " + hasFurTwo + " fur. Moreover, it has 2 wings and can fly.";
+
+        return animal;
+
+    }
 
 }
--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,5 +1,15 @@
 package com.epam.mjc.stage0;
 
-public class Dog {
+public class Dog extends Animal {
+
+    public Dog() {
+
+        super.setColor("brown");
+
+        super.setNumberOfPaws(4);
+
+        super.setHasFur(true);
+
+    }
 
 }
